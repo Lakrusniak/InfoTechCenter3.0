205ae5445435ec7a939e4d20d83d063d19dd6dde
Landon krusniak <lakrusniak@gmail.com>
2022-10-03 08:07:37 -0400

    Add print statements to Welcome Visitors

---
+++ operation_Fury.py
@@ -0,0 +1,13 @@
+#Developer: Landon Krusniak
+#Version: 1.0
+
+"""
+Our Welcome Screen will start our program letting drivers know that the InfoTechCenter OS Loading.
+"""
+
+
+
+print("\n\nWelcome to Operation Fury InfoTech Center")
+
+print("\nOperation fury's System is Booting Up")
+
