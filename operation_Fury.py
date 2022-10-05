b0c1b8c76d97e5744c16b76da32be31fd7d8e738
Landon krusniak <lakrusniak@gmail.com>
2022-10-05 08:38:17 -0400

    Version 1.0 updated Description

---
+++ operation_Fury.py
@@ -0,0 +1,11 @@
+#Wether
+#Developer Landon Krusniak
+#Version 1.0
+
+"""
+We are createong a funtion for out current wether using the
+random.choice function to determine wht the wether is
+pickingfrom a list-using If, Elif &else statemants to check the condition and print a specifc print line 
+"""
+
+
