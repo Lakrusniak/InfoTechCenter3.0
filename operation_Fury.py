d532309842c63d43d9597899a4a724bea4479402
Landon krusniak <lakrusniak@gmail.com>
2022-10-13 07:55:24 -0400

    Finished using the elif,else statements

--- operation_Fury.py
+++ operation_Fury.py
@@ -25,12 +25,24 @@ def ListOFGasstations():
 gasLevelIndicator = gas_levelGauge()
 
 def gasLevelAlert():
-    milesToGasStation = round(random.uniform(1, 25), 1)
+    miles_To_Gas_Station_Low = round(random.uniform(1, 25), 1)
+    miles_To_Gas_StationQuater_Tank = round(random.uniform(26, 50), 1)
     if gasLevelIndicator == "Empty":
-        print("***WARNING GO GET CAS YOUR WILL RUN OUT***\nCalling Rich Friend")
+        print("***WARNING GO GET GAS YOUR WILL RUN OUT***\nCalling Rich Friend")
     elif gasLevelIndicator == "Low":
         print("Run it to Empty\n Check google maps for gas stations")
         print("The closest gas station is",ListOFGasstations())
-        print("The closest gas station is",ListOFGasstations(),"which is",milesToGasStation,"miles.")
+        print("The closest gas station is",ListOFGasstations(),"which is",miles_To_Gas_Station_Low,"miles.")
+    elif gasLevelIndicator == "Quarter Tank":
+        print("***WARNING***")
+        print("Your gas tank is a quarter tank full, checking google maps to your nearest gas station")
+        print("The closest gas station is", ListOFGasstations(), "which is", miles_To_Gas_StationQuater_Tank,
+              "miles away.")
+    elif gasLevelIndicator == "Half Tank":
+        print("Your gas tank is at half of a tank and you have plenty of gas.")
+    elif gasLevelIndicator == "Three Quarter Tank":
+        print("Your gas tank is at a quarter of a tank and you have A good amount of gas to get you a couple of days")
+    else:
+        print("Your gas tank is full and you have plenty to drive a week without stopping .")
 
 gasLevelAlert()
